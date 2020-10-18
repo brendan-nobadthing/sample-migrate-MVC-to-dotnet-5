d3fc33eb99d6841ea450294ff1dac58f2751e2e2
Brendan Richards <brendan@nobadthing.com>
2020-10-18 21:44:47 +1000

    added asp.net core project

---
+++ src/WebCore/Controllers/WeatherController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Weather;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebCore.Models;
+
+namespace WebCore.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public WeatherController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: Weather
+        public ActionResult Index()
+        {
+            return View("Index", new WeatherVm()
+            {
+                WeatherRequest = new WeatherRequest()
+            });
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> GetWeather(WeatherVm reqModel)
+        {
+
+            var resultModel = new WeatherVm()
+            {
+                WeatherRequest = reqModel.WeatherRequest,
+                WeatherData = await _mediator.Send(reqModel.WeatherRequest)
+            };
+
+            return View("Index", resultModel);
+
+        }
+
+    }
+}
---
+++ src/WebCore/Models/WeatherVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Weather;
+
+namespace WebCore.Models
+{
+    public class WeatherVm
+    {
+        public WeatherRequest WeatherRequest { get; set; }
+
+        public WeatherData WeatherData { get; set; }
+    }
+}
